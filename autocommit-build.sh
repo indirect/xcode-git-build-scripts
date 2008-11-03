5d22b8c8c6776f9522e281242d239e1545c93ebb
Andre Arko <andre@arko.net>
2008-11-02 17:49:09 -0800

    First commit

---
+++ autocommit-build.sh
@@ -0,0 +1,18 @@
+# Xcode git autocommit script by Andre Arko
+#   This script will automatically commit any changed files to the project's
+#   git repository, in the branch "builds"
+
+# If you use this script, you can create more typical commits in the master
+# branch with the commands:
+#   git checkout master
+#   git merge builds --squash
+
+if [ -z "`git branch | grep builds`" ]; then
+  git co -b builds
+else
+  git co builds
+fi
+
+git commit -a -m "`date +'Build on %F at %r'`"
+# ignore git-commit exiting >0, since nothing to commit is fine
+exit 0
---
+++ git-version.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+# Xcode auto-versioning script for git by Andre Arko
+#   This script will set your CFBundleVersion number to the name of the most recent
+#   git tag, and include the current commit's distance from that tag and sha1 hash
+
+# With tag "v1.0", and two commits since then, the build will be:
+#   v1.0 (b2 abc123)
+
+# Create new versions with "git tag <version> -m <message>"
+
+# based on the ruby script by Abizern which was
+# based on the git script by Marcus S. Zarra and Matt Long which was
+# based on the Subversion script by Axel Andersson
+
+gitnum = `/usr/bin/env git describe --long`.chomp.split("-")
+
+version = gitnum[0] + " (b#{gitnum[1]} #{gitnum[2]})"
+
+info_file = ENV['BUILT_PRODUCTS_DIR'] + "/" + ENV['INFOPLIST_PATH']
+info = File.open(info_file, "r").read
+
+version_re = /([\t ]+CFBundleVersion<\/key>\n[\t ]+).*?(<\/string>)/
+info =~ version_re
+bundle_version_string = $1 + version + $2
+
+info.gsub!(version_re, bundle_version_string)
+File.open(info_file, "w") { |file| file.write(info) }
+puts "Set version string to '#{version}'"
